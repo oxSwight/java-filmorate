58dea60db0843e04589cc733464b3054ebbfa68c
IgorZ <igorzomm3r@gmail.com>
2025-04-04 20:53:58 +0200

    Внес поправки!

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,3 @@
-INSERT INTO users (name, login, email, birthday) VALUES
-('Иван Иванов', 'ivan123', 'ivan@example.com', '1990-01-01'),
-('Мария Петрова', 'masha_p', 'maria@example.com', '1992-02-02'),
-('Алексей Смирнов', 'lexa_s', 'alex@example.com', '1988-03-03');
-
-INSERT INTO friendship (user_id, friend_id, status) VALUES
-(1, 2, 'CONFIRMED'),
-(2, 1, 'CONFIRMED');
-
-INSERT INTO friendship (user_id, friend_id, status) VALUES
-(1, 3, 'CONFIRMED'),
-(3, 1, 'CONFIRMED');
-
-INSERT INTO friendship (user_id, friend_id, status) VALUES
-(2, 3, 'CONFIRMED'),
-(3, 2, 'CONFIRMED');
 
 INSERT INTO genre (name) VALUES ('Комедия');
 INSERT INTO genre (name) VALUES ('Драма');
